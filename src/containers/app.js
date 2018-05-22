92b0633ef38f1f0202a33fa9a66881f68761934b
code-guy21 <alexissj16@knights.ucf.edu>
2018-05-22 10:22:49 -0400

    Youtube Clone Created with React.js

--- src/containers/app.js
+++ src/containers/app.js
@@ -3,6 +3,7 @@ import SearchBar from '../components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from '../components/video_list';
 import VideoDetail from '../components/video_detail';
+import _ from 'lodash';
 
 const API_KEY = 'AIzaSyDPIIc3wPn14oeiYqqWabJd3K128rOczZ0';
 
